0230ec8b2860e3f3d02dd266f4f120723301eaaa
SrayanBhattacharya <bhattacharyasrayan@gmail.com>
2025-10-06 05:29:34 +0000

    Removed dist from repo

--- backend/dist/controllers/productController.js
+++
@@ -1,2 +0,0 @@
-export const getAllProducts = async () => { };
-export const createProduct = async () => { };
--- backend/dist/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllProducts, createProduct, } from "../controllers/productController.js";
-const router = express.Router();
-// GET ALL PRODUCTS
-router.get("/", getAllProducts);
-// CREATE A PRODUCT
-router.post("/", createProduct);
-export default router;
